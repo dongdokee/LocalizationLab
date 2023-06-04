22c35ce2b8d72db1b8f0ea6520940e665f889c89
dongdokee <dongdokee@naver.com>
2023-06-05 07:00:57 +0900

    .gitignore is now working

--- lib/wifi_scan.py
+++
@@ -1,9 +0,0 @@
-# TODO: WiFi (below code)
-# sudo apt-get install libatlas-base-dev
-
-#import rssi
-
-#interface = 'wlan0'
-#rssi_scanner = rssi.RSSI_Scan(interface)
-
-#fuck = rssi_scanner.getAPinfo(sudo=True)
